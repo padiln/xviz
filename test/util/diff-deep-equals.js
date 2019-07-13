a47e3fa2fa1973aa4d383d8e0f173e458105490a
Tim Wojtaszek <twojtasz@uber.com>
2019-07-13 16:22:47 -0700

    Replace deepEquals with copy that uses decimal difference of 1e-7 inequality

--- test/util/diff-deep-equals.js
+++ test/util/diff-deep-equals.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {default as deepEquals} from 'deep-equal';
+import {default as deepEquals} from './deep-equals';
 import {diffString} from 'json-diff';
 
 /**
